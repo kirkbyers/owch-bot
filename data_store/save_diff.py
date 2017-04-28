8a0c4fa724a7dba865ca1ca0a80406c6faf46c7c
Kirk byers <kingbyers@gmail.com>
2017-04-28 14:06:08 -0500

    Add saving diffs to db.

---
+++ data_store/save_diff.py
@@ -0,0 +1,22 @@
+import os
+import psycopg2
+import json
+
+from overwatch_api.get_bt_stats import getStats
+
+pgUser = os.environ['PGUSER']
+pgPass = os.environ['PGPASS']
+pgHost = os.environ['PGHOST']
+pgPort = os.environ['PGPORT']
+pgDbName = os.environ['PGDBNAME']
+
+def save_diff(battleTag, diff):
+  '''Save a previously calculated diff in db'''
+  if length(diff) > 0:
+    conn = psycopg2.connect(dbname=pgDbName, user=pgUser, password=pgPass, host=pgHost, port=pgPort)
+    cur = conn.cursor()
+    cur.execute("INSERT INTO diffs (battleTag, data) VALUES (%s, %s)", (battleTag, json.dumps(diff, ensure_ascii=False)))
+    conn.commit()
+    cur.close()
+    conn.close()
+  return diff
--- owch_discord/listen.py
+++ owch_discord/listen.py
@@ -6,6 +6,7 @@ from owch_discord.build_stats_message import printStats
 from overwatch_api.snapshot_bt import snapshot_comp
 from data_store.get_snapshots import get_last_snapshots, get_last_2_snapshots
 from data_store.diff_snapshots import diff_snapshots
+from data_store.save_diff import save_diff
 
 discordClient = discord.Client()
 
@@ -19,6 +20,7 @@ async def on_message(message):
       oldStats = get_last_snapshots(messageContentArray[1])[0][0]
       stats = snapshot_comp(messageContentArray[1])
       diff = diff_snapshots(stats, oldStats)
+      save_diff(messageContentArray[1], diff)
       for formatedMessage in printStats(diff):
         await discordClient.send_message(message.author, formatedMessage)
     elif message.content.startswith('!latest'):
