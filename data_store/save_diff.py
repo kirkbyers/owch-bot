7221ab31d9d2f78ece55c34deb0e1ec8eb1baea6
Kirk byers <kingbyers@gmail.com>
2017-04-28 14:52:13 -0500

    Fix len typo.

--- data_store/save_diff.py
+++ data_store/save_diff.py
@@ -12,7 +12,7 @@ pgDbName = os.environ['PGDBNAME']
 
 def save_diff(battleTag, diff):
   '''Save a previously calculated diff in db'''
-  if length(diff) > 0:
+  if len(diff) > 0:
     conn = psycopg2.connect(dbname=pgDbName, user=pgUser, password=pgPass, host=pgHost, port=pgPort)
     cur = conn.cursor()
     cur.execute("INSERT INTO diffs (battleTag, data) VALUES (%s, %s)", (battleTag, json.dumps(diff, ensure_ascii=False)))
